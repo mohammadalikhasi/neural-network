4c9a5b194f516f93f6bd0a5a0ab19757313dfb02
mohammad <mohammadalikhasi2002@gmail.com>
2023-05-05 19:14:32 +0430

    add matplot and error calculation

--- neural.py
+++ neural.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import math as ms
 
 df = pd.read_csv('trainhouse.csv')
 
@@ -9,25 +11,39 @@ inputs = np.array(df.loc[:,['area','bedrooms','bathrooms','stories','parking']])
 expected = np.array(df.loc[:,'price'])
 
 
-weights = np.array([10000,1000000,1000000,3000000,2000000])
+weights = np.array([20000,500000,500000,500000,500000])
 bias = 1000000
 
-print(inputs[0][1])
 epoch = 0
-
+MSE = []
 while epoch < 100000:
     output = np.dot(inputs,weights) + bias
     mse = output - expected
-    print(weights)
+    mse_amount = 0
+    if epoch < 200:
+        for i in mse:
+            mse_amount += i**2
+        MSE.append(mse_amount)
     for i in range(5):
         weight_change = 0
         for j in range(len(mse)):
             weight_change += 2*mse[j]*inputs[j][i]
         if (i == 0):
-            weights[i] -= 0.00000000001*weight_change
+            weights[i] -= 0.000000000001*weight_change
         else:
              weights[i] -= 0.0000001*weight_change
     epoch+=1
 
-print(output)
-print(np.sum(mse))
+print(f'final weights are : {weights}')
+print(f'this model has {ms.fabs(np.sum(mse))*10} error on train')
+df2 = pd.read_csv('test.csv')
+inputs = np.array(df2.loc[:,['area','bedrooms','bathrooms','stories','parking']])
+expected = np.array(df2.loc[:,'price'])
+output = np.dot(inputs,weights) + bias
+mse = output - expected
+
+
+print(f'this model has {np.sum(mse)} error on test')
+
+plt.plot(MSE)
+plt.show()
